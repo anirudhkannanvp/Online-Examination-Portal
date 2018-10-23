a7f43a615464f7a190970587797c0839347307bd
Subhadeep <dashsubhadeep@gmail.com>
2018-10-23 19:06:10 +0530

    Templates

--- online_exam/views.py
+++ online_exam/views.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import course, user, topic, subtopic, question_type, level, exam_detail, question_bank,  option, answer, registration, result
 
+def home(request):
+    return render(request ,'online_exam/home.html')
 # Create your views here.
