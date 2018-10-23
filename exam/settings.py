62ebe72dd2abfa36ee462f006cd5c06f2f42b659
Subhadeep <dashsubhadeep@gmail.com>
2018-10-23 20:01:59 +0530

    Templates

--- exam/settings.py
+++ exam/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1ufm$hp_)5g4q-nrx^)!8wav8n7$z-*oh(2j)fh#fx8ybk!_r3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -123,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
