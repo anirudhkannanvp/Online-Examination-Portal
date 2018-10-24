aec2552e15c7b2ed059464a6a4fe174185f96235
Subhadeep <dashsubhadeep@gmail.com>
2018-10-24 13:32:22 +0530

    Template for student and admin

--- online_exam/urls.py
+++ online_exam/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^home/$', views.home, name='home'),
     url(r'^student/$', views.student, name='student'),
 ]
