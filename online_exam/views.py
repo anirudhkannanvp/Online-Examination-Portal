335b62be09ee8117e6559458bf3eed310f2092bf
Subhadeep <dashsubhadeep@gmail.com>
2018-10-24 14:23:14 +0530

    Templates

--- online_exam/views.py
+++ online_exam/views.py
@@ -5,8 +5,81 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import course, user, topic, subtopic, question_type, level, exam_detail, question_bank,  option, answer, registration, result
 
-def home(request):
-    return render(request ,'online_exam/home.html')
+def faculty_dashboard(request):
+    return render(request ,'online_exam/faculty_dashboard.html')
+    
+def faculty_add_course(request):
+    return render(request ,'online_exam/faculty_add_course.html')
+
+def faculty_add_exam(request):
+    return render(request ,'online_exam/faculty_add_exam.html')
+
+def faculty_add_topic(request):
+    return render(request ,'online_exam/faculty_add_topic.html')
+
+def faculty_add_subtopic(request):
+    return render(request ,'online_exam/faculty_add_subtopic.html')
+
+def faculty_add_question(request):
+    return render(request ,'online_exam/faculty_add_question.html')
+
+def faculty_modify_course(request):
+    return render(request ,'online_exam/faculty_modify_course.html')
+
+def faculty_modify_exam(request):
+    return render(request ,'online_exam/faculty_modify_exam.html')
+
+def faculty_modify_topic(request):
+    return render(request ,'online_exam/faculty_modify_topic.html')
+
+def faculty_modify_subtopic(request):
+    return render(request ,'online_exam/faculty_modify_subtopic.html')
+
+def faculty_modify_question(request):
+    return render(request ,'online_exam/faculty_modify_question.html')
+
+def faculty_update_course(request):
+    return render(request ,'online_exam/faculty_update_course.html')
+
+def faculty_update_exam(request):
+    return render(request ,'online_exam/faculty_update_exam.html')
+
+def faculty_update_topic(request):
+    return render(request ,'online_exam/faculty_update_topic.html')
+
+def faculty_update_subtopic(request):
+    return render(request ,'online_exam/faculty_update_subtopic.html')
+
+def faculty_update_question(request):
+    return render(request ,'online_exam/faculty_update_question.html')
+
+def faculty_view_courses(request):
+    return render(request ,'online_exam/faculty_view_courses.html')
+
+def faculty_view_exams(request):
+    return render(request ,'online_exam/faculty_view_exams.html')
+
+def faculty_view_topics(request):
+    return render(request ,'online_exam/faculty_view_topics.html')
+
+def faculty_view_subtopics(request):
+    return render(request ,'online_exam/faculty_view_subtopics.html')
+
+def faculty_view_questions(request):
+    return render(request ,'online_exam/faculty_view_questions.html')
+
+def faculty_evaluate(request):
+    return render(request ,'online_exam/faculty_evaluate.html')
+
+def faculty_exam_registrations(request):
+    return render(request ,'online_exam/faculty_exam_registrations.html')
+
+def faculty_user_registrations(request):
+    return render(request ,'online_exam/faculty_user_registrations.html')
+
+def faculty_profile(request):
+    return render(request ,'online_exam/faculty_profile.html')
+                           
 # Create your views here.
-def student(request):
-	return render(request, 'online_exam/student_home.html')
+def student_dashboard(request):
+	return render(request, 'online_exam/student_dashboard.html')
